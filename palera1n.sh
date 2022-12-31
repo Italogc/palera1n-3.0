59c5f5d8148ff25bebf512b4cebf4d259263664e
Italogc <igctgrabgr@gmail.com>
2022-12-31 15:45:37 -0300

    Update palera1n.sh

--- palera1n.sh
+++ palera1n.sh
@@ -36,7 +36,7 @@ _wait() {
     if [ "$1" = 'normal' ]; then
         if [ "$os" = 'Darwin' ]; then
             if ! (system_profiler SPUSBDataType 2> /dev/null | grep 'Manufacturer: Apple Inc.' >> /dev/null); then
-                echo "[*] Waiting for idevice in normal mode"
+                echo "[*] Aguardando aparelho em modo normal sem senhas de bloqueio"
             fi
 
             while ! (system_profiler SPUSBDataType 2> /dev/null | grep 'Manufacturer: Apple Inc.' >> /dev/null); do
@@ -44,7 +44,7 @@ _wait() {
             done
         else
             if ! (lsusb 2> /dev/null | grep ' Apple, Inc.' >> /dev/null); then
-                echo "[*] Waiting for device in normal mode"
+                echo "[*] Aguardando aparelho em modo normal sem senhas de bloqueio"
             fi
 
             while ! (lsusb 2> /dev/null | grep ' Apple, Inc.' >> /dev/null); do
@@ -54,7 +54,7 @@ _wait() {
     elif [ "$1" = 'recovery' ]; then
         if [ "$os" = 'Darwin' ]; then
             if ! (system_profiler SPUSBDataType 2> /dev/null | grep ' Apple Mobile Device (Recovery Mode):' >> /dev/null); then
-                echo "[*] Waiting for idevice to reconnect in recovery mode"
+                echo "[*] Aguardando aparelho para reconectar em modo recuperacao"
             fi
 
             while ! (system_profiler SPUSBDataType 2> /dev/null | grep ' Apple Mobile Device (Recovery Mode):' >> /dev/null); do
@@ -62,7 +62,7 @@ _wait() {
             done
         else
             if ! (lsusb 2> /dev/null | grep 'Recovery Mode' >> /dev/null); then
-                echo "[*] Waiting for idevice to reconnect in recovery mode"
+                echo "[*] Aguardando aparelho para reconectar em modo recuperacao"
             fi
 
             while ! (lsusb 2> /dev/null | grep 'Recovery Mode' >> /dev/null); do
@@ -88,12 +88,12 @@ _wait() {
 _check_dfu() {
     if [ "$os" = 'Darwin' ]; then
         if ! (system_profiler SPUSBDataType 2> /dev/null | grep ' Apple Mobile Device (DFU Mode):' >> /dev/null); then
-            echo "[-] Connected idevice is not in DFU mode, please rerun the script and try again"
+            echo "[-] Aparelho conectado nao esta em Modo DFU, Favor reexecute este script e tente novamente quando estiver em DFU"
             exit
         fi
     else
         if ! (lsusb 2> /dev/null | grep 'DFU Mode' >> /dev/null); then
-            echo "[-] Connected idevice is not in DFU mode, please rerun the script and try again"
+            echo "[-] Aparelho conectado nao esta em Modo DFU, Favor reexecute este script e tente novamente quando estiver em DFU"
             exit
         fi
     fi
@@ -119,23 +119,23 @@ _pwn() {
 }
 
 _reset() {
-        echo "[*] Resetting DFU state"
+        echo "[*] Retirando do estado DFU"
         "$dir"/gaster reset
 }
 
 _dfuhelper() {
-    echo "[*] Please press any key when ready for DFU mode"
+    echo "[*] Por favor clique em qualquer tecla para botar em modo DFU"
     read -n 1 -s
-    step 3 "Get ready"
-    step 4 "Hold volume down + side button" &
+    step 3 "Iniciar"
+    step 4 "Segure juntos o botao de diminuir volume + botao de desligar" &
     sleep 3
     "$dir"/irecovery -c "reset"
-    step 1 "Keep holding"
-    step 10 'Release side button, but keep holding volume down'
+    step 1 "Continue segurando, nao solte ainda..."
+    step 10 'Agora solte o botao de desligar, mas continue segurando o botao de diminuir volume'
     sleep 1
     
     _check_dfu
-    echo "[*] idevice entered DFU!"
+    echo "[*] Aparelho em modo DFU com sucesso!"
 }
 
 _kill_if_running() {
@@ -170,7 +170,7 @@ _exit_handler() {
         fi
     fi
     [ $? -eq 0 ] && exit
-    echo "[-] An error occurred"
+    echo "[-] Ocorreu um erro"
 
     cd logs
     for file in *.log; do
@@ -178,7 +178,7 @@ _exit_handler() {
     done
     cd ..
 
-    echo "[*] A failure log has been made. If you're going to make a GitHub issue, please attach the latest log."
+    echo "[*] Arquivo de log de falha foi criado. Para ajudar envie-nos seu arquivo de log pelo Github para correcao futura."
 }
 trap _exit_handler EXIT
 
@@ -199,17 +199,17 @@ fi
 
 if [ "$1" = 'clean' ]; then
     rm -rf boot* work .tweaksinstalled
-    echo "[*] Removed the created boot files"
+    echo "[*] Arquivos criados de boot removidos com sucesso"
     exit
 elif [ "$1" = 'dfuhelper' ]; then
-    echo "[*] Running DFU helper"
+    echo "[*] Executando ajudante de DFU"
     _dfuhelper
     exit
 elif [ "$1" = '--restorerootfs' ]; then
-    echo "[*] Restoring rootfs..."
+    echo "[*] Restaurando seu sistema root aos padroes de fabrica..."
     "$dir"/irecovery -n
     sleep 2
-    echo "[*] Done, your idevice will boot into iOS now."
+    echo "[*] Sucesso, agora seu aparelho rebootara normalmente."
     # clean the boot files bcs we don't need them anymore
     rm -rf boot-"$deviceid" work .tweaksinstalled
     exit
@@ -233,7 +233,7 @@ fi
 
 # Check for pyimg4
 if ! python3 -c 'import pkgutil; exit(not pkgutil.find_loader("pyimg4"))'; then
-    echo '[-] pyimg4 not installed. Press any key to install it, or press ctrl + c to cancel'
+    echo '[-] pyimg4 nao esta instalado. Pressione qualquer tecla para instalar, ou segure CTRL + C para cancelar'
     read -n 1 -s
     python3 -m pip install pyimg4
 fi
@@ -262,13 +262,13 @@ chmod +x "$dir"/*
 # Start
 # ============
 
-echo "palera1n | Version $version-$branch-$commit"
-echo "Written by Nebula and Mineek | Some code and ramdisk from Nathan | Loader app by Amy modified by pwn2e"
+echo "palera1n | Versao $version-$branch-$commit"
+echo "Editado por Nebula e Mineek | Mais codigos de ramdisk por Nathan | Aplicativo Loader por Amy, modificado br por iTalogc iOS"
 echo ""
 
 if [ ! "$1" = '--tweaks' ] && [[ "$@" == *"--semi-tethered"* ]]; then
-    echo "[!] --semi-tethered may not be used with rootless"
-    echo "    Rootless is already semi-tethered"
+    echo "[!] --semi-tethered não será usado com modo rootless"
+    echo "    Palera1n rootless será do tipo semi-tethered"
     exit
 fi
 
@@ -277,17 +277,17 @@ if [ "$1" = '--tweaks' ]; then
 fi
 
 if [ "$1" = '--tweaks' ] && [ ! -e ".tweaksinstalled" ] && [ ! -e ".disclaimeragree" ] && [[ ! "$@" == *"--semi-tethered"* ]]; then
-        echo "!!! WARNING WARNING WARNING !!!"
-    echo "THIS ONLY WORKS ON 15.0-15.7.1"
-    echo "DO NOT GET ANGRY AT US IF UR iDEVICE IS BORKED, IT'S YOUR OWN FAULT AND WE WARNED YOU"
-    echo "DO YOU UNDERSTAND? TYPE 'Yes, pwn my idevice' TO CONTINUE"
+        echo "!!! AVISO AVISO AVISO !!!"
+    echo "ESTA FERRAMENTA FUNCIONA APENAS COM CHIPS A8x-A11 COM IOS 15.0-16.3!"
+    echo "NAO NOS RESPONSABILIZAMOS CASO SEU APARELHO BRICKE, ESTE É NOSSO ÚLTIMO AVISO!"
+    echo "VOCE TEM CERTEZA? DIGITE 'sim' E TECLE ENTER PARA CONTINUAR COM O JAILBREAK"
     read -r answer
-    if [ "$answer" = 'Yes, pwn my idevice' ]; then
-        echo "Are you REALLY sure? WE WARNED YOU!"
-        echo "Type 'Yes, do as I say' to continue"
+    if [ "$answer" = 'sim' ]; then
+        echo "VOCE REALMENTE TEM CERTEZA? NOS TE AVISAMOS!"
+        echo "DIGITE 'sim' E TECLE ENTER PARA CONTINUAR"
         read -r answer
-        if [ "$answer" = 'Yes, do as I say' ]; then
-            echo "[*] Enabling tweaks"
+        if [ "$answer" = 'sim' ]; then
+            echo "[*] Habilitando tweaks"
             tweaks=1
             touch .disclaimeragree
         else
@@ -301,7 +301,7 @@ fi
 # Get device's iOS version from ideviceinfo if in normal mode
 if [ "$1" = '--dfu' ] || [ "$1" = '--tweaks' ]; then
     if [ -z "$2" ]; then
-        echo "[-] When using --dfu, please pass the version you're device is on"
+        echo "[-] Se usar --dfu, por favor digite na frente a versao do ios do seu aparelho"
         exit
     else
         version=$2
@@ -311,18 +311,18 @@ else
     version=$(_info normal ProductVersion)
     arch=$(_info normal CPUArchitecture)
     if [ "$arch" = "arm64e" ]; then
-        echo "[-] palera1n doesn't, and never will, work on non-checkm8 devices"
+        echo "[-] o palera1n nao, e jamais, funcionara com aparelhos A12 ou superior!"
         exit
     fi
     echo "Hello, $(_info normal ProductType) on $version!"
 
-    echo "[*] Switching idevice into recovery mode..."
+    echo "[*] Botando seu aparelho em modo recuperacao..."
     "$dir"/ideviceenterrecovery $(_info normal UniqueDeviceID)
     _wait recovery
 fi
 
 # Grab more info
-echo "[*] Getting idevice info..."
+echo "[*] Resgatando informacoes do aparelho..."
 cpid=$(_info recovery CPID)
 model=$(_info recovery MODEL)
 deviceid=$(_info recovery PRODUCT)
@@ -348,10 +348,10 @@ if [ ! -f blobs/"$deviceid"-"$version".shsh2 ]; then
 
     cd ramdisk
     chmod +x sshrd.sh
-    echo "[*] Creating ramdisk"
+    echo "[*] Criando seu ramdisk"
     ./sshrd.sh 15.6 `if [ ! "$1" = '--tweaks' ]; then echo "rootless"; fi`
 
-    echo "[*] Booting ramdisk"
+    echo "[*] Bootando seu ramdisk"
     ./sshrd.sh boot
     cd ..
     # if known hosts file exists, remove it
@@ -374,7 +374,7 @@ if [ ! -f blobs/"$deviceid"-"$version".shsh2 ]; then
         sleep 1
     done
 
-    echo "[*] Dumping blobs and installing Pogo"
+    echo "[*] Despejando os blobs e instalando o Pogo"
     sleep 1
     "$dir"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "/usr/bin/mount_filesystems"
     "$dir"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "cat /dev/rdisk1" | dd of=dump.raw bs=256 count=$((0x4000)) 
@@ -382,7 +382,7 @@ if [ ! -f blobs/"$deviceid"-"$version".shsh2 ]; then
     rm dump.raw
 
     if [[ "$@" == *"--semi-tethered"* ]]; then
-        echo "[*] Creating fakefs, this may take a while (up to 10 minutes)"
+        echo "[*] Clonando sistema root, isto pode demorar (mais de 10 minutos)"
         "$dir"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "/sbin/newfs_apfs -A -D -o role=r -v System /dev/disk0s1"
         sleep 2
         if [[ "$@" == *"--no-baseband"* ]]; then 
@@ -394,14 +394,14 @@ if [ ! -f blobs/"$deviceid"-"$version".shsh2 ]; then
         sleep 1
         "$dir"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "cp -a /mnt1/. /mnt8/"
         sleep 1
-        echo "[*] fakefs created, continuing..."
+        echo "[*] sistema root clonado com sucesso, continuando..."
     fi
 
     if [[ ! "$@" == *"--no-install"* ]]; then
         tipsdir=$("$dir"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "/usr/bin/find /mnt2/containers/Bundle/Application/ -name 'Tips.app'" 2> /dev/null)
         sleep 1
         if [ "$tipsdir" = "" ]; then
-            echo "[!] Tips is not installed. Once your device reboots, install Tips from the App Store and retry"
+            echo "[!] Aplicativo Dicas (Tips) nao instalado. Apos seu aparelho reiniciar instale este aplicativo na AppStore e tente novamente"
             "$dir"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "/sbin/reboot"
             sleep 1
             _kill_if_running iproxy
@@ -426,9 +426,9 @@ if [ ! -f blobs/"$deviceid"-"$version".shsh2 ]; then
 
     has_active=$("$dir"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "ls /mnt6/active" 2> /dev/null)
     if [ ! "$has_active" = "/mnt6/active" ]; then
-        echo "[!] Active file does not exist! Please use SSH to create it"
-        echo "    /mnt6/active should contain the name of the UUID in /mnt6"
-        echo "    When done, type reboot in the SSH session, then rerun the script"
+        echo "[!] Arquivo de ativacao inexistente! Favor use um cliente de SSH para criar"
+        echo "    /mnt6/active devera conter o nome do seu UUID em /mnt6"
+        echo "    Quando terminar, reinicie sua sessao SSH, e reexecute este script"
         echo "    ssh root@localhost -p 2222"
         exit
     fi
@@ -439,14 +439,14 @@ if [ ! -f blobs/"$deviceid"-"$version".shsh2 ]; then
     "$dir"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "/bin/chmod 755 /mnt1/private/var/root/Kernel15Patcher.ios"
 
     # lets actually patch the kernel
-    echo "[*] Patching the kernel"
+    echo "[*] Patcheando o kernel..."
     "$dir"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "rm -f /mnt6/$active/System/Library/Caches/com.apple.kernelcaches/kcache.raw /mnt6/$active/System/Library/Caches/com.apple.kernelcaches/kcache.patched /mnt6/$active/System/Library/Caches/com.apple.kernelcaches/kcache.im4p /mnt6/$active/System/Library/Caches/com.apple.kernelcaches/kernelcachd"
     "$dir"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "cp /mnt6/$active/System/Library/Caches/com.apple.kernelcaches/kernelcache /mnt6/$active/System/Library/Caches/com.apple.kernelcaches/kernelcache.bak"
     sleep 1
     # download the kernel
-    echo "[*] Downloading BuildManifest"
+    echo "[*] Baixando o BuildManifest..."
     "$dir"/pzb -g BuildManifest.plist "$ipswurl"
-    echo "[*] Downloading kernelcache"
+    echo "[*] Baixando os Caches de Kernel..."
     "$dir"/pzb -g "$(awk "/""$cpid""/{x=1}x&&/kernelcache.release/{print;exit}" BuildManifest.plist | grep '<string>' | cut -d\> -f2 | cut -d\< -f1)" "$ipswurl"
     mv kernelcache.release.* work/kernelcache
     if [[ "$deviceid" == "iPhone8"* ]] || [[ "$deviceid" == "iPad6"* ]]|| [[ "$deviceid" == *'iPad5'* ]]; then
@@ -473,16 +473,16 @@ if [ ! -f blobs/"$deviceid"-"$version".shsh2 ]; then
     sleep 1
     has_kernelcachd=$("$dir"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "ls /mnt6/$active/System/Library/Caches/com.apple.kernelcaches/kernelcachd" 2> /dev/null)
     if [ "$has_kernelcachd" = "/mnt6/$active/System/Library/Caches/com.apple.kernelcaches/kernelcachd" ]; then
-        echo "[*] Custom kernelcache now exists!"
+        echo "[*] Caches de Kernel customizados criados com sucesso!"
     else
-        echo "[!] Custom kernelcache doesn't exist..? Please send a log and report this bug..."
+        echo "[!] Caches de Kernel nao existem...? Favor nos envie os seus logs deste problema pelo Github para fix futuro..."
     fi
 
     rm -rf work
     mkdir work
 
     sleep 2
-    echo "[*] Done! Rebooting your idevice"
+    echo "[*] Sucesso! Rebootando o seu aparelho..."
     "$dir"/sshpass -p 'alpine' ssh -o StrictHostKeyChecking=no -p2222 root@localhost "/sbin/reboot"
     sleep 1
     _kill_if_running iproxy
@@ -491,13 +491,13 @@ if [ ! -f blobs/"$deviceid"-"$version".shsh2 ]; then
         _wait normal
         sleep 5
 
-        echo "[*] Switching idevice into recovery mode..."
+        echo "[*] Botando seu aparelho em Modo Recuperacao..."
         "$dir"/ideviceenterrecovery $(_info normal UniqueDeviceID)
     elif [ ! "$1" = '--tweaks' ]; then
         _wait normal
         sleep 5
 
-        echo "[*] Switching idevice into recovery mode..."
+        echo "[*] Botando seu aparelho em Modo Recuperacao..."
         "$dir"/ideviceenterrecovery $(_info normal UniqueDeviceID)
     fi
     _wait recovery
@@ -520,22 +520,22 @@ if [ ! -f boot-"$deviceid"/ibot.img4 ]; then
     rm -rf boot-"$deviceid"
     mkdir boot-"$deviceid"
 
-    echo "[*] Converting blob"
+    echo "[*] Convertendo blobs..."
     "$dir"/img4tool -e -s $(pwd)/blobs/"$deviceid"-"$version".shsh2 -m work/IM4M
     cd work
 
-    echo "[*] Downloading BuildManifest"
+    echo "[*] Baixando seu BuildManifest..."
     "$dir"/pzb -g BuildManifest.plist "$ipswurl"
 
-    echo "[*] Downloading and decrypting iBSS"
+    echo "[*] Baixando e descriptografando seu iBSS..."
     "$dir"/pzb -g "$(awk "/""$cpid""/{x=1}x&&/iBSS[.]/{print;exit}" BuildManifest.plist | grep '<string>' | cut -d\> -f2 | cut -d\< -f1)" "$ipswurl"
     "$dir"/gaster decrypt "$(awk "/""$cpid""/{x=1}x&&/iBSS[.]/{print;exit}" BuildManifest.plist | grep '<string>' | cut -d\> -f2 | cut -d\< -f1 | sed 's/Firmware[/]dfu[/]//')" iBSS.dec
 
-    echo "[*] Downloading and decrypting iBoot"
+    echo "[*] Baixando e descriptografando seu iBoot..."
     "$dir"/pzb -g "$(awk "/""$cpid""/{x=1}x&&/iBoot[.]/{print;exit}" BuildManifest.plist | grep '<string>' | cut -d\> -f2 | cut -d\< -f1)" "$ipswurl"
     "$dir"/gaster decrypt "$(awk "/""$cpid""/{x=1}x&&/iBoot[.]/{print;exit}" BuildManifest.plist | grep '<string>' | cut -d\> -f2 | cut -d\< -f1 | sed 's/Firmware[/]all_flash[/]//')" ibot.dec
 
-    echo "[*] Patching and signing iBSS/iBoot"
+    echo "[*] Patcheando e assinando iBSS/iBoot"
     "$dir"/iBoot64Patcher iBSS.dec iBSS.patched
     if [[ "$@" == *"--semi-tethered"* ]]; then
         if [[ "$@" == *"--no-baseband"* ]]; then 
@@ -566,7 +566,7 @@ fi
 sleep 2
 _pwn
 _reset
-echo "[*] Booting idevice"
+echo "[*] Bootando o aparelho..."
 if [[ "$cpid" == *"0x801"* ]]; then
     sleep 1
     "$dir"/irecovery -f boot-"$deviceid"/ibot.img4
@@ -597,11 +597,19 @@ cd ..
 
 rm -rf work rdwork
 echo ""
-echo "Done!"
-echo "The idevice should now verboose into iOS"
-echo "If this is your first time jailbreaking, open palera1n app and press Install"
-echo "Otherwise, open palera1n app and press activate tweaks then respring in the settings section"
-echo "This version of palera1n is a fork by @pwnd2e"
-echo "Enjoy!"
+echo "Jailbrek Finalizado com Sucesso!"
+echo "Seu Dispositivo será agora patcheado com seu iOS"
+echo "Por seguranca mantenha seu Aparelho conectado via cabo USB neste terminal enquanto acaba de concluir os procedimentos finais abaixo"
+echo "Se essa for sua primeira vez com este jailbreak, abra o aplicativo do Palerain e clique no botao INSTALL"
+echo "Após isso, abra o aplicativo Palera1n novamente e clique em DO ALL nos ajustes e espere seu aparelho dar respring"
+echo "Se aparecer uma janela de opcoes na tela do seu aparelho escolha ALWAYS ALLOW"
+echo "Agora ja pode desplugar o aparelho do seu computador e usar normalmente"
+echo "Esta versão do Palera1n foi desenvolvida por @pwnd2e e traduzida pt-BR por iTalogc iOS"
+echo "A partir de agora, todas as vezes que desligar seu aparelho, coloque ele em modo DFU e faca jailbreak por este programa"
+echo "Para isso, use os comandos abaixo no terminal:"
+echo "cd palera1n-3.0"
+echo "sudo ./palera1n.sh --tweaks sua_versao_ios"
+echo "Exemplo: sudo ./palera1n.sh --tweaks 15.3.1"
+echo "Divirta-se com moderacao!
 
 } | tee logs/"$(date +%T)"-"$(date +%F)"-"$(uname)"-"$(uname -r)".log
